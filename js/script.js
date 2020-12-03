51338128d5926a63e6b9a067a17a27ce0f8538ae
Dario <darioalessio79@gmail.com>
2020-12-03 15:28:30 +0100

    prove

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var app = new Vue({
     inputSearch:''
   },
   mounted: function (){
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=f162c76f293b24b89a2c277c7f09142c')
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=f162c76f293b24b89a2c277c7f09142c&query=ritorno+al+futuro')
     .then(film => {
       this.arrayMovie = film.data.results
     });
