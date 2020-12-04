5279b43df75eac5b56507d707a62d76a5967cf89
Dario <darioalessio79@gmail.com>
2020-12-04 15:25:18 +0100

    1 Milestone

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 
+
 var app = new Vue({
   el:"#app",
   data:{
     arrayMovie:[],
     inputSearch:''
   },
-  mounted: function (){
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=f162c76f293b24b89a2c277c7f09142c&query=ritorno+al+futuro')
-    .then(film => {
-      this.arrayMovie = film.data.results
-    });
+  methods:{
+    search(){
+      // ('URI + HTTP / ? API-KEY + &query=' + params)
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=f162c76f293b24b89a2c277c7f09142c&query=' + this.inputSearch)
+      .then(response => {
+        this.arrayMovie = response.data.results;
+          this.inputSearch = '';
+      });
+
+    }
   }
+
 });
